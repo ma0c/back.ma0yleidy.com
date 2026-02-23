512678c445215081bd2971aa132e664c3d5ffdd6
Mauricio Collazos <mauricio@lextegrity.com>
2026-02-23 12:24:06 -0500

    Adding debugging print

--- applications/confirmation/viewsets.py
+++ applications/confirmation/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, mixins as viewsets_mixins
 
 from applications.confirmation import serializers
+from rest_framework.response import Response
 
 class ConfirmationViewSet(
     viewsets_mixins.RetrieveModelMixin,
@@ -10,3 +11,20 @@ class ConfirmationViewSet(
     serializer_class = serializers.ConfirmationSerializer
     queryset = serializers.ConfirmationSerializer.Meta.model.objects.all()
     lookup_field = 'slug'
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        print("Request data:")
+        print(request.data)
+        print("=================================")
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
